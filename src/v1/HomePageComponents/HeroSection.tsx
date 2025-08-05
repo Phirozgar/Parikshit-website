c47d5daee6254fd4a7324a822eedfedfdbacc331
nxndinisxh <nandini1.mitmpl2024@learner.manipal.edu>
2025-08-05 19:16:50 +0530

    Updated the JoinUs Button on the Home Page

--- src/v1/HomePageComponents/HeroSection.tsx
+++ src/v1/HomePageComponents/HeroSection.tsx
@@ -1,6 +1,10 @@
 import { ChevronRight } from "lucide-react";
+import { JoinUsModal } from "./JoinUsModal";
+import { useState } from "react";
 
 export function HeroSection() {
+  const [showJoinModal, setShowJoinModal ] =useState(false);
+
   return (
     <section id="home" className="relative min-h-screen">
       {/* Background Video */}
@@ -26,12 +30,16 @@ export function HeroSection() {
           </h1>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 mt-32">
             <button className="bg-[#7AECEC] text-black px-6 py-3 rounded-full font-bold
-                              hover:bg-white transition-colors flex items-center">
+                              hover:bg-white transition-colors flex items-center"
+                              onClick={() => setShowJoinModal(true)}
+                              >
               JOIN US<ChevronRight className="ml-0.5" />
             </button>
           </div>
         </div>
       </div>
+
+      <JoinUsModal open={showJoinModal} onClose={() => setShowJoinModal(false)} />
     </section>
   );
 }
