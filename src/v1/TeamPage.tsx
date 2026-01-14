dca5ca6c36c743e15eba041b94cc2d18caf81130
nxndinisxh <nandini1.mitmpl2024@learner.manipal.edu>
2026-01-14 17:28:05 +0530

    Added all the remaining pictures

--- src/v1/TeamPage.tsx
+++ src/v1/TeamPage.tsx
@@ -50,6 +50,7 @@ const allTeamMembers: TeamMember[] = [
   { name: "Nandini Sah", role: "Administration Team Member", image: getImagePath("Nandini") },
   { name: "Rishitha Kantevari", role: "Administration Team Member", image: getImagePath("Rishitha") },
 
+  // Faculty Advisors
   { name: "Dr. Shreesha Chokkadi", role: "Faculty Advisor",image: getImagePath("Shreesha"),
     department: "Electrical & Instrumentation Engineering" },
   { name: "Dr. Balbir Singh", role: "Faculty Advisor", image: getImagePath("Balbir"),
